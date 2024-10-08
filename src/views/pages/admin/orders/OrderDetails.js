2f726e85c5113a901e68d0c48601333b438029a3
Shibli <shiblirz92@gmail.com>
2024-10-08 11:58:14 +0530

    Fix: reported issues

--- src/views/pages/admin/orders/OrderDetails.js
+++ src/views/pages/admin/orders/OrderDetails.js
@@ -1,87 +1,99 @@
 // ** MUI Imports
-import Paper from '@mui/material/Paper'
-import Table from '@mui/material/Table'
-import TableRow from '@mui/material/TableRow'
-import TableHead from '@mui/material/TableHead'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableContainer from '@mui/material/TableContainer'
-import WarningDialog from 'src/views/components/dialogs/WarningDialog'
-import { useState } from 'react'
-import { CardHeader } from '@mui/material'
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableRow from "@mui/material/TableRow";
+import TableHead from "@mui/material/TableHead";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import WarningDialog from "src/views/components/dialogs/WarningDialog";
+import { useState } from "react";
+import { CardHeader } from "@mui/material";
 
 const createData = (name, calories, fat, carbs, protein) => {
-  return { name, calories, fat, carbs, protein }
-}
+  return { name, calories, fat, carbs, protein };
+};
 
-const groupByWeek = items => {
-  const grouped = {}
-  items?.forEach(item => {
+const groupByWeek = (items) => {
+  const grouped = {};
+  items?.forEach((item) => {
     if (!grouped[item.week]) {
-      grouped[item.week] = []
+      grouped[item.week] = [];
     }
-    grouped[item.week].push(item)
-  })
-  return grouped
-}
+    grouped[item.week].push(item);
+  });
+  return grouped;
+};
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9)
-]
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
 
 const OrderDetails = ({ order }) => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
-  let groupedItems = groupByWeek(order?.items)
+  let groupedItems = groupByWeek(order?.items);
   return (
     <TableContainer component={Paper}>
-      <CardHeader title='View Order' />
-      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+      <CardHeader title="View Order" />
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Week</TableCell>
-            <TableCell align='left'>Date</TableCell>
-            <TableCell align='left'>Item</TableCell>
-            <TableCell align='left'>Category</TableCell>
-            <TableCell align='left'>Qty</TableCell>
-            <TableCell align='left'>Price</TableCell>
+            <TableCell align="left">Date</TableCell>
+            <TableCell align="left">Item</TableCell>
+            <TableCell align="left">Category</TableCell>
+            <TableCell align="left">Qty</TableCell>
+            <TableCell align="left">Price</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {Object?.keys(groupedItems).map((week, index) => {
-            const weekItems = groupedItems[week]
+            const weekItems = groupedItems[week];
             return weekItems.map((item, idx) => (
               <TableRow key={item.id}>
                 {/* Row span for the first item of each week */}
-                {idx === 0 && <TableCell rowSpan={weekItems.length}>Week {week}</TableCell>}
-                <TableCell align='left'>
-                  {new Date(item.date).toLocaleDateString('en-GB', {
-                    weekday: 'long' // "Wednesday"
+                {idx === 0 && (
+                  <TableCell rowSpan={weekItems.length}>Week {week}</TableCell>
+                )}
+                <TableCell align="left">
+                  {new Date(item.date).toLocaleDateString("en-GB", {
+                    weekday: "long", // "Wednesday"
                   })}
                   <br />
-                  {new Date(item.date).toLocaleDateString('en-GB', {
+                  {new Date(item.date).toLocaleDateString("en-GB", {
                     // "Wednesday"
-                    day: 'numeric', // "10"
-                    month: 'short', // "May"
-                    year: 'numeric' // "2024"
+                    day: "numeric", // "10"
+                    month: "short", // "May"
+                    year: "numeric", // "2024"
                   })}
                 </TableCell>
-                <TableCell align='left' sx={{ fontFamily: 'DM Sans', fontWeight: '600' }}>
+                <TableCell
+                  align="left"
+                  sx={{ fontFamily: "DM Sans", fontWeight: "600" }}
+                >
                   {item.name}
                 </TableCell>
-                <TableCell align='left'>{item.type}</TableCell>
-                <TableCell align='left'>{item.quantity}</TableCell>
-                <TableCell align='left' sx={{ fontFamily: 'DM Sans', color: '#0A5247 !important', fontWeight: '900' }}>
-                  £{item.price * item.quantity}
+                <TableCell align="left">{item.type}</TableCell>
+                <TableCell align="left">{item.quantity}</TableCell>
+                <TableCell
+                  align="left"
+                  sx={{
+                    fontFamily: "DM Sans",
+                    color: "#0A5247 !important",
+                    fontWeight: "900",
+                  }}
+                >
+                  £{item.price}
                 </TableCell>
               </TableRow>
-            ))
+            ));
           })}
           {/* {order?.items?.map((row, index) => {
             return (
@@ -123,12 +135,12 @@ const OrderDetails = ({ order }) => {
       <WarningDialog
         open={open}
         page={1}
-        title={'Are you sure'}
-        message={'This will delete everything'}
+        title={"Are you sure"}
+        message={"This will delete everything"}
         setOpen={setOpen}
       />
     </TableContainer>
-  )
-}
+  );
+};
 
-export default OrderDetails
+export default OrderDetails;
