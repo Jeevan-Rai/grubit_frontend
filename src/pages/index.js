48d3c5ee07811bc4776e618d4107209bcc81f73a
Shibli Raja <shiblirz92@gmail.com>
2024-11-30 19:30:45 +0530

    Fix: reported issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,14 +24,16 @@ const Home = () => {
       <Box sx={{ padding: { xs: '20px', md: '30px' } }} />
       <WhyChooseUs />
       <Box sx={{ padding: { xs: '20px', md: '30px' } }} />
-      <Testimonials />
+      <OurPhilosophy />
+      
       <Box sx={{ padding: { xs: '20px', md: '30px' } }} id='about' />
+      <TrainStation />
+      <Box sx={{ padding: { xs: '20px', md: '30px' } }} />
       <AboutUs />
       
       <Box sx={{ padding: { xs: '20px', md: '30px' } }} />
-      <OurPhilosophy />
-      <Box sx={{ padding: { xs: '20px', md: '30px' } }} />
-      <TrainStation />
+      <Testimonials />
+      
       <Box sx={{ padding: { xs: '20px', md: '30px' } }} id='contact' />
       <ContactUs />
       <Box sx={{ padding: { xs: '20px', md: '30px' } }} />
